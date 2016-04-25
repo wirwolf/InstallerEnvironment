3b144e522c8f3a60666cfe1adf119e6f4077154f
Andru Cherny <wir_wolf@bk.ru>
2016-04-25 11:27:06 +0300

    Update installer.sh

--- ubuntu_server/installer.sh
+++ ubuntu_server/installer.sh
@@ -60,7 +60,9 @@ sudo apt-get --yes --force-yes upgrade
 
 #------------------------------add nodejs repo--------------------------------------
 sudo apt-get --yes --force-yes install curl
-curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
 #-----------------------------------------------------------------------------------
 
 sudo apt-get update
