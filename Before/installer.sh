104102f6e15eab3b1fb1a19d644f9e2aaf1be65d
Andru Cherny <wir_wolf@bk.ru>
2016-06-15 16:50:58 +0300

    Update installer.sh

--- Before/installer.sh
+++ Before/installer.sh
@@ -5,59 +5,54 @@ export HOME_USER=user
 . /etc/lsb-release
  
 sudo cat > /etc/apt/sources.list <<EOF
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME main restricted
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME main restricted
- 
-## Major bug fix updates produced after the final release of the
-## distribution.
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-updates main restricted
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-updates main restricted
- 
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team. Also, please note that software in universe WILL NOT receive any
-## review or updates from the Ubuntu security team.
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME universe
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME universe
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-updates universe
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-updates universe
- 
-## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
-## team, and may not be under a free licence. Please satisfy yourself as to
-## your rights to use the software. Also, please note that software in
-## multiverse WILL NOT receive any review or updates from the Ubuntu
-## security team.
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME multiverse
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME multiverse
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-updates multiverse
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-updates multiverse
- 
-## Uncomment the following two lines to add software from the 'backports'
-## repository.
-## N.B. software from this repository may not have been tested as
-## extensively as that contained in the main release, although it includes
-## newer versions of some applications which may provide useful features.
-## Also, please note that software in backports WILL NOT receive any review
-## or updates from the Ubuntu security team.
-# deb http://ua.archive.ubuntu.com/ubuntu/ $DISTRIB_CODENAME-backports main restricted universe multiverse
-# deb-src http://ua.archive.ubuntu.com/ubuntu/ $DISTRIB_CODENAME-backports main restricted universe multiverse
- 
-## Uncomment the following two lines to add software from Canonical's
-## 'partner' repository.
-## This software is not part of Ubuntu, but is offered by Canonical and the
-## respective vendors as a service to Ubuntu users.
+#------------------------------------------------------------------------------#
+#                            OFFICIAL UBUNTU REPOS                             #
+#------------------------------------------------------------------------------#
+
+
+###### Ubuntu Main Repos
+deb http://ubuntu.volia.net/ubuntu/ $DISTRIB_CODENAME main restricted universe multiverse 
+deb-src http://ubuntu.volia.net/ubuntu/ $DISTRIB_CODENAME main restricted universe multiverse 
+
+###### Ubuntu Update Repos
+deb http://ubuntu.volia.net/ubuntu/ $DISTRIB_CODENAME-security main restricted universe multiverse 
+deb http://ubuntu.volia.net/ubuntu/ $DISTRIB_CODENAME-updates main restricted universe multiverse 
+deb-src http://ubuntu.volia.net/ubuntu/ $DISTRIB_CODENAME-security main restricted universe multiverse 
+
+###### Ubuntu Partner Repo
 deb http://archive.canonical.com/ubuntu $DISTRIB_CODENAME partner
-# deb-src http://archive.canonical.com/ubuntu $DISTRIB_CODENAME partner
- 
-## This software is not part of Ubuntu, but is offered by third-party
-## developers who want to ship their latest software.
+deb-src http://archive.canonical.com/ubuntu $DISTRIB_CODENAME partner
+
+###### Ubuntu Extras Repo
 deb http://extras.ubuntu.com/ubuntu $DISTRIB_CODENAME main
 deb-src http://extras.ubuntu.com/ubuntu $DISTRIB_CODENAME main
- 
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-security main restricted
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-security main restricted
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-security universe
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-security universe
-deb http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-security multiverse
-deb-src http://mirror.mirohost.net/ubuntu/ $DISTRIB_CODENAME-security multiverse
+
+#------------------------------------------------------------------------------#
+#                           UNOFFICIAL UBUNTU REPOS                            #
+#------------------------------------------------------------------------------#
+
+
+###### 3rd Party Binary Repos
+
+#### Kubuntu Beta Backports PPA - https://launchpad.net/~kubuntu-ppa/+archive/beta
+## Run this command: sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8AC93F7A
+deb http://ppa.launchpad.net/kubuntu-ppa/beta/ubuntu $DISTRIB_CODENAME main
+
+#### Kubuntu Updates PPA - https://edge.launchpad.net/~kubuntu-ppa/+archive/ppa
+## Run this command: sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8AC93F7A
+deb http://ppa.launchpad.net/kubuntu-ppa/ppa/ubuntu $DISTRIB_CODENAME main 
+
+
+####### 3rd Party Source Repos
+
+#### Kubuntu Beta Backports PPA (Source) - https://launchpad.net/~kubuntu-ppa/+archive/beta
+## Run this command: sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8AC93F7A
+deb-src http://ppa.launchpad.net/kubuntu-ppa/beta/ubuntu $DISTRIB_CODENAME main
+
+#### Kubuntu Updates PPA (Source) - https://edge.launchpad.net/~kubuntu-ppa/+archive/ppa
+## Run this command: sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8AC93F7A
+deb-src http://ppa.launchpad.net/kubuntu-ppa/ppa/ubuntu $DISTRIB_CODENAME main 
 EOF
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8AC93F7A
 sudo apt-get update
